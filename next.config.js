67662c71090553061d63a9fbdbf9ea7c5cc90f2e
lisy999 <2574561698@qq.com>
2024-07-11 16:25:55 +0800

    层级文件显示中文

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  navs: [
+    {
+      url: "https://github.com/shuding/nextra",
+      name: "Nextwwwwwwwwwwwwwwwwsra",
+    },
+  ],
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
