6ca5b935c75ce215b55faa8b5fc20360972094a9
lisy999 <159228560+lisy999@users.noreply.github.com>
2024-07-14 14:50:18 +0000

    feat shanchu

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  navs: [
-    {
-      url: "https://github.com/shuding/nextra",
-      name: "Nextwwwwwwwwwwwwwwwwsra",
-    },
-  ],
 });
 
 module.exports = withNextra();
